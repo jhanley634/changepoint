8e0e910c152af9fa15ac62b1e384014725fca575
John Hanley <jhanley@rexhomes.com>
2021-03-14 13:21:22 -0700

    Add random noise

--- ch2_detector/view_ols.py
+++ ch2_detector/view_ols.py
@@ -1,10 +1,13 @@
-#! /usr/bin/env steamlit run
+#! /usr/bin/env streamlit run
 
 import numpy as np
 import pandas as pd
-import statsmodels.api as sm
+# import statsmodels.api as sm
 import statsmodels.formula.api as smf
 import streamlit as sl
+from numpy.random import default_rng
+
+rng = default_rng(seed=42)
 
 
 def get_line(m=.6, b=7, n_pts=1000):
@@ -15,16 +18,15 @@ def get_line(m=.6, b=7, n_pts=1000):
                         for x in np.linspace(0, 1, n_pts))
 
 
-def ols(eps=.1):
+def ols(eps=.5):
     """Ordinary Least Squares regression demo.
     """
     df = get_line()
+    df.y += eps * rng.standard_normal(size=len(df))
 
-    # Fit regression model (using the natural log of one of the regressors)
-    results = smf.ols('y ~ x', data=df).fit()
+    model = smf.ols('y ~ x', data=df).fit()
 
-    print(results.summary())
-
+    print(model.summary())
 
 
 if __name__ == '__main__':
