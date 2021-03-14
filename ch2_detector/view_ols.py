53c83baf3644f8036b39299951508d49d05c9df8
John Hanley <jhanley@rexhomes.com>
2021-03-14 12:49:44 -0700

    Add statsmodel OLS

--- ch2_detector/view_ols.py
+++ ch2_detector/view_ols.py
@@ -2,11 +2,24 @@
 
 import numpy as np
 import pandas as pd
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
 import streamlit as sl
 
 
-def ols():
-    ''
+def ols(m=1, b=0, eps=.1):
+    """Ordinary Least Squares regression demo.
+
+    y = m x + b + epsilon_noise
+    """
+    dat = sm.datasets.get_rdataset("Guerry", "HistData").data
+    print(dat)
+
+    # Fit regression model (using the natural log of one of the regressors)
+    results = smf.ols('Lottery ~ Literacy + np.log(Pop1831)', data=dat).fit()
+
+    print(results.summary())
+
 
 
 if __name__ == '__main__':
