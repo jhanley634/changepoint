97fdfb456e51d78eccee8f516b3503e71d6f18da
John Hanley <jhanley@rexhomes.com>
2021-03-21 10:00:55 -0700

    Add get_curve

--- auto/auto_sklearn.py
+++ auto/auto_sklearn.py
@@ -6,9 +6,20 @@ from autosklearn.regression import AutoSklearnRegressor
 from sklearn.datasets import load_boston
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
+import numpy as np
 import pandas as pd
 
 from viz.pd_prof import get_us_df
+
+
+def pnomial(x, a=0, b=0, c=4, d=5):
+    return a * x**3 + b * x**2 + c * x + d
+
+
+def get_curve(lo=0, hi=100, n_samples=1000, sigma=.5):
+    df = pd.DataFrame(dict(x=np.linspace(lo, hi, n_samples)))
+    df['y'] = df.x.apply(pnomial) + sigma * np.random.standard_normal(n_samples)
+    return df
 
 
 def main():
@@ -17,17 +28,22 @@ def main():
     # X = df.date
     # y = df.deaths
 
-    boston_data = load_boston()
-    features = pd.DataFrame(boston_data.data, columns=boston_data.feature_names)
-    target = pd.DataFrame(boston_data.target, columns=['TARGET'])
-    # dataset = pd.concat([features, target], axis=1)
+    df = get_curve()
+    features = pd.DataFrame(df, columns=['x'])
+    target = pd.DataFrame(df, columns=['y'])
     xtrain, xtest, ytrain, ytest = train_test_split(features, target, test_size=0.2)
+    print(ytrain)
+    print(ytest)
 
-    regressor = AutoSklearnRegressor(time_left_for_this_task=60)
+    regressor = AutoSklearnRegressor(time_left_for_this_task=60, n_jobs=1)
     regressor.fit(xtrain, ytrain)
-    print(regressor.sprint_statistics())
     print(regressor.show_models())
+    print(regressor.sprint_statistics())
 
+    df = get_curve(100, 200)
+    features = pd.DataFrame(df, columns=['x'])
+    target = pd.DataFrame(df, columns=['y'])
+    xtrain, xtest, ytrain, ytest = train_test_split(features, target, test_size=0.2)
     pred = regressor.predict(xtest)
     mae = mean_absolute_error(ytest, pred)
     print("MAE:", mae)
